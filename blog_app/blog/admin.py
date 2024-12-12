754dd2c76523bc42bb52f33198824d8c8ac9fa24
Naveen Kumar-2024-aib <naveen13524g@gmail.com>
2024-12-12 21:44:13 +0530

    Customized admin page

--- blog_app/blog/admin.py
+++ blog_app/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Post,Catagory
 # Register your models here.
+
+class Postadmin(admin.ModelAdmin):
+    list_display = ('title','content')
+    search_fields = ('title', 'content')
+    list_filter = ('created_at_time','catagory')
+
+
+admin.site.register(Post,Postadmin)
+admin.site.register(Catagory)
